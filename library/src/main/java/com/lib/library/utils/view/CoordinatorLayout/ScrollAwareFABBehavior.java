f7bf7e2874e5c57e21bf68a34d3ff7bdace92b86
zhongjh <254191389@qq.com>
2018-02-23 17:32:55 +0800

    改善日历布局，修改fab因为7.0如果fab隐藏了，就不起效的问题

--- library/src/main/java/com/lib/library/utils/view/CoordinatorLayout/ScrollAwareFABBehavior.java
+++ library/src/main/java/com/lib/library/utils/view/CoordinatorLayout/ScrollAwareFABBehavior.java
@@ -24,6 +24,7 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
         super();
     }
 
+
     @Override
     public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout, final FloatingActionButton child,
                                        final View directTargetChild, final View target, final int nestedScrollAxes) {
@@ -37,10 +38,12 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
                                final View target, final int dxConsumed, final int dyConsumed,
                                final int dxUnconsumed, final int dyUnconsumed) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-        if (dyConsumed > 0 && !this.mIsAnimatingOut && child.getVisibility() == View.VISIBLE) {
+        if (dyConsumed > 0 && !this.mIsAnimatingOut) {
+//        if (dyConsumed > 0 && !this.mIsAnimatingOut && child.getVisibility() == View.VISIBLE) {
             // User scrolled down and the FAB is currently visible -> hide the FAB
             animateOut(child);
-        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+//        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+        } else if (dyConsumed < 0) {
             // User scrolled up and the FAB is currently not visible -> show the FAB
             animateIn(child);
         }
@@ -61,7 +64,7 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
 
                         public void onAnimationEnd(View view) {
                             ScrollAwareFABBehavior.this.mIsAnimatingOut = false;
-                            view.setVisibility(View.GONE);
+//                            view.setVisibility(View.GONE);
                         }
                     }).start();
         } else {
@@ -71,7 +74,7 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
 
     // Same animation that FloatingActionButton.Behavior uses to show the FAB when the AppBarLayout enters
     private void animateIn(FloatingActionButton button) {
-        button.setVisibility(View.VISIBLE);
+//        button.setVisibility(View.VISIBLE);
         if (Build.VERSION.SDK_INT >= 14) {
             ViewCompat.animate(button).translationY(0)
                     .setInterpolator(INTERPOLATOR).withLayer().setListener(null)
