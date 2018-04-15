043c8bf7fc527c9461174610660fdd4ba8e02b17
台式电脑 <254191389@qqcom>
2018-04-16 02:26:36 +0800

    修复销毁时自动保存的问题

---
+++ library/src/main/java/com/lib/library/widget/TabLayoutUtils.java
@@ -0,0 +1,24 @@
+package com.lib.library.widget;
+
+import android.support.design.widget.TabLayout;
+import android.widget.LinearLayout;
+
+/**
+ * TabLayout控件
+ */
+public class TabLayoutUtils {
+
+    /**
+     * 设置启用/禁用点击
+     *
+     * @param tabLayout
+     */
+    public static void setEnable(TabLayout tabLayout, boolean isEnable) {
+        LinearLayout tabStrip = ((LinearLayout) tabLayout.getChildAt(0));
+        for (int k = 0; k < tabStrip.getChildCount(); k++) {
+            // 因为所有触摸事件，返回false都是继续下一个的，所以跟enable是相反的
+            tabStrip.getChildAt(k).setOnTouchListener((v, event) -> !isEnable);
+        }
+    }
+
+}
