1564012ab3c107eefac2d3e61df02f0d5e41c145
aaatttcccc <254191389@qq.com>
2019-05-13 23:55:55 +0800

    添加备份，修改缓存机制

--- library/src/main/java/com/lib/library/utils/displaymetrics/DisplayMetricsSPUtils.java
+++ library/src/main/java/com/lib/library/utils/displaymetrics/DisplayMetricsSPUtils.java
@@ -6,7 +6,6 @@ import android.view.WindowManager;
 
 import com.lib.library.utils.constants.ScreenConstants;
 import com.lib.library.utils.storage.PreferenceUtils;
-import com.lib.library.utils.storage.SPUtils;
 
 /**
  * 获取手机分辨率的宽高，先从缓存获取，如果缓存都没有，则重新计算，并且保存
@@ -27,10 +26,10 @@ public class DisplayMetricsSPUtils {
             wm.getDefaultDisplay().getSize(size);
             int screenWidth = size.x;
             int screenHeight = size.y;
-            SPUtils.getInstance().put(ScreenConstants.ScreenHeight, screenHeight);
-            SPUtils.getInstance().put(ScreenConstants.ScreenWidth, screenWidth);
+            PreferenceUtils.getInstance(context).saveParam(ScreenConstants.ScreenHeight, screenHeight);
+            PreferenceUtils.getInstance(context).saveParam(ScreenConstants.ScreenWidth, screenWidth);
         }
-        return SPUtils.getInstance().getInt(ScreenConstants.ScreenHeight, 0);
+        return PreferenceUtils.getInstance(context).getIntParam(ScreenConstants.ScreenHeight, 0);
     }
 
     /**
@@ -40,16 +39,16 @@ public class DisplayMetricsSPUtils {
      * @return 宽
      */
     public static int getScreenWidth(Context context) {
-        if (SPUtils.getInstance().getInt(ScreenConstants.ScreenWidth, 0) == 0) {
+        if (PreferenceUtils.getInstance(context).getIntParam(ScreenConstants.ScreenWidth, 0) == 0) {
             WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
             Point size = new Point();
             wm.getDefaultDisplay().getSize(size);
             int screenWidth = size.x;
             int screenHeight = size.y;
-            SPUtils.getInstance().put(ScreenConstants.ScreenHeight, screenHeight);
-            SPUtils.getInstance().put(ScreenConstants.ScreenWidth, screenWidth);
+            PreferenceUtils.getInstance(context).saveParam(ScreenConstants.ScreenHeight, screenHeight);
+            PreferenceUtils.getInstance(context).saveParam(ScreenConstants.ScreenWidth, screenWidth);
         }
-        return SPUtils.getInstance().getInt(ScreenConstants.ScreenWidth, 0);
+        return PreferenceUtils.getInstance(context).getIntParam(ScreenConstants.ScreenWidth, 0);
     }
 
 }
