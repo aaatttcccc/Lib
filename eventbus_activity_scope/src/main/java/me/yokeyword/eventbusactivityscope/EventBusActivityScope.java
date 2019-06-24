f83bdf13a212e9c047b00fd086270a6b759d5874
zhongjh <254191389@qq.com>
2019-06-24 15:44:52 +0800

    继续完善日夜间模式

--- eventbus_activity_scope/src/main/java/me/yokeyword/eventbusactivityscope/EventBusActivityScope.java
+++ eventbus_activity_scope/src/main/java/me/yokeyword/eventbusactivityscope/EventBusActivityScope.java
@@ -30,48 +30,47 @@ public class EventBusActivityScope {
         if (sInitialized.getAndSet(true)) {
             return;
         }
-// TODO
-//        ((Application) context.getApplicationContext())
-//                .registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
-//                    private Handler mainHandler = new Handler(Looper.getMainLooper());
-//
-//                    @Override
-//                    public void onActivityCreated(Activity activity, Bundle bundle) {
-//                        sActivityEventBusScopePool.put(activity, new LazyEventBusInstance());
-//                    }
-//
-//                    @Override
-//                    public void onActivityStarted(Activity activity) {
-//                    }
-//
-//                    @Override
-//                    public void onActivityResumed(Activity activity) {
-//                    }
-//
-//                    @Override
-//                    public void onActivityPaused(Activity activity) {
-//                    }
-//
-//                    @Override
-//                    public void onActivityStopped(Activity activity) {
-//                    }
-//
-//                    @Override
-//                    public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
-//                    }
-//
-//                    @Override
-//                    public void onActivityDestroyed(final Activity activity) {
-//                        if (!sActivityEventBusScopePool.containsKey(activity)) return;
-//
-//                        mainHandler.post(new Runnable() { // Make sure Fragment's onDestroy() has been called.
-//                            @Override
-//                            public void run() {
-//                                sActivityEventBusScopePool.remove(activity);
-//                            }
-//                        });
-//                    }
-//                });
+        ((Application) context.getApplicationContext())
+                .registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+                    private Handler mainHandler = new Handler(Looper.getMainLooper());
+
+                    @Override
+                    public void onActivityCreated(Activity activity, Bundle bundle) {
+                        sActivityEventBusScopePool.put(activity, new LazyEventBusInstance());
+                    }
+
+                    @Override
+                    public void onActivityStarted(Activity activity) {
+                    }
+
+                    @Override
+                    public void onActivityResumed(Activity activity) {
+                    }
+
+                    @Override
+                    public void onActivityPaused(Activity activity) {
+                    }
+
+                    @Override
+                    public void onActivityStopped(Activity activity) {
+                    }
+
+                    @Override
+                    public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+                    }
+
+                    @Override
+                    public void onActivityDestroyed(final Activity activity) {
+                        if (!sActivityEventBusScopePool.containsKey(activity)) return;
+
+                        mainHandler.post(new Runnable() { // Make sure Fragment's onDestroy() has been called.
+                            @Override
+                            public void run() {
+                                sActivityEventBusScopePool.remove(activity);
+                            }
+                        });
+                    }
+                });
     }
 
     /**
