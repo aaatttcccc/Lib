9a305721a970d8f60ef37449822341dda0042772
zhongjh <Qq20249019qQ>
2020-03-31 00:06:15 +0800

    去掉分发Fragment的onActivityResult事件，不知道会不会影响别的

--- library/src/main/java/com/lib/library/phone/BaseActivity.java
+++ library/src/main/java/com/lib/library/phone/BaseActivity.java
@@ -35,42 +35,6 @@ public abstract class BaseActivity extends MySupportActivity {
         onInitListener();
     }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        for (int indext = 0; indext < fragmentManager.getFragments().size(); indext++) {
-            Fragment fragment = fragmentManager.getFragments().get(indext); //找到第一层Fragment
-            if (fragment == null)
-                Log.w(TAG, "Activity result no fragment exists for index: 0x"
-                        + Integer.toHexString(requestCode));
-            else
-                handleResult(fragment, requestCode, resultCode, data);
-        }
-    }
-
-    /**
-     * 递归调用，对所有子Fragement生效
-     *
-     * @param frag        递归的fragment
-     * @param requestCode 请求码
-     * @param resultCode  返回码
-     * @param data        数据源
-     */
-    private void handleResult(Fragment frag, int requestCode, int resultCode,
-                              Intent data) {
-        frag.onActivityResult(requestCode & 0xffff, resultCode, data);
-        List<Fragment> frags = frag.getChildFragmentManager().getFragments();
-        if (frags != null) {
-            for (Fragment f : frags) {
-                if (f != null)
-                    handleResult(f, requestCode, resultCode, data);
-            }
-        }
-    }
-
-
     @Override
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
